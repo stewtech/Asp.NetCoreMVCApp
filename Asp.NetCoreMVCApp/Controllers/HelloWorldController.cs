2c709467715a27f07535e95e6f36ac75344160c3
Cecil Stewart <72238656+stewtech@users.noreply.github.com>
2022-12-18 13:27:16 -0600

    Add project files.

---
+++ Asp.NetCoreMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCoreMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCoreMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
